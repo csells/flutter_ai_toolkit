8aeb206f193d9deeced825e4e2e0255bcd3a1a65
Chris Sells <csells@sellsbrothers.com>
2024-09-03 17:11:10 -0700

    simplifed the sample

--- example/lib/constants.dart
+++
@@ -1,4 +0,0 @@
-enum ProviderType { echo, firebaseVertext, googleGemini }
-
-// Set the provider type to use
-const providerType = ProviderType.firebaseVertext;
--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,23 +1,21 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_ai_toolkit/flutter_ai_toolkit.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-import 'chat_page.dart';
-import 'constants.dart';
 import 'firebase_options.dart'; // from `flutterfire config`
+
+const kUseFirebase = false;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  switch (providerType) {
-    case ProviderType.firebaseVertext:
-      await Firebase.initializeApp(
-        options: DefaultFirebaseOptions.currentPlatform,
-      );
-    case ProviderType.googleGemini:
-      await dotenv.load();
-    case ProviderType.echo:
-      break;
+  if (kUseFirebase) {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+  } else {
+    await dotenv.load();
   }
 
   runApp(const App());
@@ -31,9 +29,49 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) => MaterialApp(
         title: title,
         home: const ChatPage(),
-        themeMode: ThemeMode.system,
         theme: ThemeData.light(),
-        darkTheme: ThemeData.dark(),
         debugShowCheckedModeBanner: false,
+      );
+}
+
+class ChatPage extends StatefulWidget {
+  const ChatPage({super.key});
+
+  @override
+  State<ChatPage> createState() => _ChatPageState();
+}
+
+class _ChatPageState extends State<ChatPage> {
+  late final LlmProvider provider;
+
+  @override
+  void initState() {
+    super.initState();
+    provider = _initProvider();
+  }
+
+  @override
+  void setState(VoidCallback fn) {
+    super.setState(() {
+      fn();
+      provider = _initProvider();
+    });
+  }
+
+  LlmProvider _initProvider() => kUseFirebase
+      ? FirebaseVertexProvider(
+          model: 'gemini-1.5-flash',
+        )
+      : GeminiProvider(
+          model: 'gemini-1.5-flash',
+          apiKey: dotenv.get('GEMINI_API_KEY'),
+        );
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          title: Text('${App.title} - ${provider.displayName}'),
+        ),
+        body: LlmChatView(provider: provider),
       );
 }
