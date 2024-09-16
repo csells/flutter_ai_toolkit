abdf530847a30b2a30a0fe167ea7871478a3ea12
Chris Sells <csells@sellsbrothers.com>
2024-09-15 20:14:32 -0700

    WIP: gemma support via ollama

---
+++ lib/src/providers/ollama_provider.dart
@@ -0,0 +1,70 @@
+// Copyright 2024 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:ollama_dart/ollama_dart.dart';
+
+import 'llm_provider_interface.dart';
+
+/// A provider for interacting with Ollama models.
+class OllamaProvider extends LlmProvider {
+  /// Creates an [OllamaProvider] instance.
+  ///
+  /// [model] is the name of the Ollama model to use.
+  /// [systemInstruction] is an optional system message to set the behavior of the model.
+  OllamaProvider({
+    required this.model,
+    this.systemInstruction = 'You are a helpful assistant.',
+  }) {
+    _messages.add(
+      Message(role: MessageRole.system, content: systemInstruction),
+    );
+  }
+
+  /// The name of the Ollama model to use.
+  final String model;
+
+  /// The system instruction used to set the behavior of the model.
+  final String systemInstruction;
+
+  final _client = OllamaClient();
+  final _messages = <Message>[];
+
+  @override
+
+  /// Generates a stream of text responses from the Ollama model.
+  ///
+  /// [prompt] is the user's input text.
+  /// [attachments] is an optional list of attachments (currently not used by Ollama).
+  ///
+  /// Returns a [Stream] of [String] containing the generated text.
+  Stream<String> generateStream(
+    String prompt, {
+    Iterable<Attachment> attachments = const [],
+  }) async* {
+    // need to add support for attachments
+    if (attachments.isNotEmpty) {
+      throw UnimplementedError('No support for attachments yet');
+    }
+
+    _messages.add(Message(role: MessageRole.user, content: prompt));
+
+    final stream = _client.generateChatCompletionStream(
+      request: GenerateChatCompletionRequest(
+        model: model,
+        messages: _messages,
+      ),
+    );
+
+    var content = '';
+    await for (final response in stream) {
+      final text = response.message.content;
+      if (text.isNotEmpty) {
+        yield text;
+        content += text;
+      }
+    }
+
+    _messages.add(Message(role: MessageRole.assistant, content: content));
+  }
+}
--- lib/src/providers/providers.dart
+++ lib/src/providers/providers.dart
@@ -6,3 +6,4 @@ export 'echo_provider.dart';
 export 'firebase_vertex_provider.dart';
 export 'gemini_provider.dart';
 export 'llm_provider_interface.dart';
+export 'ollama_provider.dart';
